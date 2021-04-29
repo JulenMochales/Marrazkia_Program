9ec6deeec8f74e2695480e6b8a614bfeef35b9c8
JulenMochales <mochales.julen@uni.eus>
2021-04-29 11:35:59 +0200

    marrazkia bukatuta

--- src/Marrazkia.java
+++ src/Marrazkia.java
@@ -9,28 +9,33 @@ public class Marrazkia {
         int altuera = 0;
         int zabalera = 0;
 
-        while (altuera == 0 || altuera < 0) {
+        if (altuera == 0) {
             System.out.println("Sartu altuera (zenbaki positiboa) ");
             altuera = in.nextInt();
 
         }
-        while (zabalera == 0 || zabalera < 0) {
+        while (altuera < 0) {
+            System.out.println("Sartu altuera (zenbaki positiboa) ");
+            altuera = in.nextInt();
+        }
+        if (zabalera == 0) {
             System.out.println("Sartu zabalera (zenbaki positiboa)");
             zabalera = in.nextInt();
 
         }
-        int bukatu = zabalera * altuera;
-        int anch = 1;
+        while (zabalera < 0) {
+            System.out.println("Sartu zabalera (zenbaki positiboa)");
+            zabalera = in.nextInt();
 
-        for (int zenbakia = 0; zenbakia < bukatu; zenbakia++) {
+        }
+        int kontadorea = 1;
 
-            System.out.println(zenbakia + 1);
-            System.out.print("  ");
-
-            if (zenbakia == zabalera) {
-                System.out.println("  ");
+        for (int i = 0; i < altuera; i++) {
+            for (int j = 0; j < zabalera; j++) {
+                System.out.print(kontadorea);
+                kontadorea++;
             }
-
+            System.out.println("");
         }
 
     }
